09a1d69926990b34adb0db43b08ead64c1a10966
Daniel W. Ottey <dan.ottey@boomi.com>
2023-07-07 22:47:03 -0400

    Making use of the database to hold the posts.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
-posts = [
-    {
-        "author": "Dan",
-        "title": "Blog Post 1",
-        "content": "First Post content",
-        "date_posted": "July 6, 2023",
-    },
-    {
-        "author": "Janet",
-        "title": "Blog Post 2",
-        "content": "Second Post content",
-        "date_posted": "July 7, 2023",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html.j2", context)
 
 
