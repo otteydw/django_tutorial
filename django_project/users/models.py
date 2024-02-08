38bad5de9ed603f1f44f09c3cb6c83c89c117fd3
Daniel W. Ottey <dan@bluefrogs.us>
2024-02-07 22:10:33 -0500

    Tutorial 09 - Update User Profile.

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    def save(self):
+        # TODO: Delete the original file before saving the new file
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        MAX_THUMBNAIL_SIZE = (300, 300)
+        if img.height > MAX_THUMBNAIL_SIZE[0] or img.width > MAX_THUMBNAIL_SIZE[1]:
+            img.thumbnail(MAX_THUMBNAIL_SIZE)
+            img.save(self.image.path)
