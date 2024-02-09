91ea83407da1cfcb594df7fc03d249af959a1858
Daniel W. Ottey <dan@bluefrogs.us>
2024-02-09 12:28:11 -0500

    Tutorial 10 - Create, Update and Delete Posts using Class Based Views. Ensure that isort uses the black profile for compatibility.

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
 from . import views
+from .views import (
+    PostCreateView,
+    PostDeleteView,
+    PostDetailView,
+    PostListView,
+    PostUpdateView,
+)
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
+    path("", PostListView.as_view(), name="blog-home"),
     path("about/", views.about, name="blog-about"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
 ]
